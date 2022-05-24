140af61e81b9d21126af7f48f5b82c51bb435314
Will-Rich-724 <williamtrich5@gmail.com>
2022-05-24 07:44:29 -0500

    corrected initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpoonForm from './components/SpoonForm';
 
 function App() {
   return (
     <div>
-      
+      <SpoonForm />
     </div>
   );
 }
--- src/components/SpoonForm.tsx
+++ src/components/SpoonForm.tsx
@@ -1,15 +1,39 @@
 import React, {useState} from 'react';
-
-interface first {first: string}
-interface last {last: string}
-interface spoonName {spoonName: string}
     
-
 const SpoonForm = () => {
-    const [firstName, setFirstName] = useState<first | null>(null)
-    const [lastName, setLastName] = useState<last | null>(null)
-    const [spoonName, setSpoonName] = useState<spoonName | null>(null)
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [spoonName, setSpoonName] = useState("")
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.id === 'first') {
+            setFirstName(e.target.value)
+        } else {
+            setLastName(e.target.value)
+        }
+    }
+    
+    const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+
+    }
+
+    return (
+        <>
+        <div>
+            <h2>Spoonerize Me</h2>
+            <form>
+                <label className="block">First Name
+                <input type="text" id="first" onChange={handleChange}/>
+                </label>
+                <label className="block">Last Name
+                <input type="text" id="last" onChange={handleChange}/>
+                </label>
+                <button type="submit" onSubmit={handleSubmit}/>
+            </form>
+            <h2>{spoonName ? spoonName : null}</h2>
+        </div>
+        </>
+    )
 }
 
 export default SpoonForm
