8894a717ca1fb0e36088cf57beedef805949d588
Will-Rich-724 <williamtrich5@gmail.com>
2022-05-24 19:07:09 -0500

    test case working

--- src/components/SpoonForm.tsx
+++ src/components/SpoonForm.tsx
@@ -13,22 +13,38 @@ const SpoonForm = () => {
         }
     }
     
-    const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
+        const isVowel = (letter : string) => !!letter.match(/[aeiou]/i)
+        const fnflVowel = isVowel(firstName[0])
+        const fnslVowel = isVowel(firstName[1])
+        const lnflVowel = isVowel(lastName[0])
+        const lnslVowel = isVowel(lastName[1])
+        let newFirst = ''
+        let newLast = ''
+
+        if (fnflVowel === false && fnslVowel === true && lnflVowel === false && lnslVowel === true) {
+            console.log('Case 1')
+        newFirst = lastName[0].concat(firstName.slice(1))
+        newLast = firstName[0].concat(lastName.slice(1))
+        }
+
+        setSpoonName(`${newFirst} ${newLast}`)
     }
 
     return (
         <>
         <div>
             <h2>Spoonerize Me</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="block">First Name
                 <input type="text" id="first" onChange={handleChange}/>
                 </label>
                 <label className="block">Last Name
                 <input type="text" id="last" onChange={handleChange}/>
                 </label>
-                <button type="submit" onSubmit={handleSubmit}/>
+                <button type="submit">Spoonerize</button>
             </form>
             <h2>{spoonName ? spoonName : null}</h2>
         </div>
